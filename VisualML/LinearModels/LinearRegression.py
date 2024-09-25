320ebf3c1117ca63bd2a69aff8f69ab50de6e164
Se00n00 <bluesky5april2021@gmail.com>
2024-09-26 03:34:46 +0530

    Added All Regularization Regression: L1, L2 and Elastic Net in LinearRegression.py and LogisticsRegression.py

--- VisualML/LinearModels/LinearRegression.py
+++ VisualML/LinearModels/LinearRegression.py
@@ -1,15 +1,32 @@
 import numpy as np
 
 class LinearRegression:
-    def __init__(self,X,Y,Lrate):
+    ''' 
+    Regression <type of Linear Regression> ::
+    Linear: Linear Regression, 
+    L1: Lasso Linear Regression, 
+    L2: Ridge Linear Regression,
+    ElasticNet: ElasticNet Linear Regression
+
+    lambd1: when [Regression='L1' || Regression='ElasticNet']
+    lambd2: when [Regression='L2' || Regression='ElasticNet']
+    '''
+    def __init__(self, X, Y, Lrate, Regression='Linear', lambd1=0.01, lambd2=0.01):
         Feature = X.shape[1]
+
+        self.Regression = Regression
+
+        #Hyper-Parameters Initiallization
+        self.Lrate = Lrate
+        self.Epochs = 100
+        self.Lambd1 = lambd1
+        self.Lambd2 = lambd2
+
+        #Parameters Initiallization
         self.Weights = np.random.rand(1,Feature)
         self.Biases = np.random.rand(1,1)
-        self.Lrate = Lrate
-        self.Epochs =100
-        print(self.Weights)
+        
         self.Train(X,Y)
-        print(self.Weights)
 
     def Train(self,X,Y):
         N = X.shape[0]
@@ -20,7 +37,15 @@ class LinearRegression:
             
             #Backward Propagation
             LOSS = Y.T-A
-            LOSSX = np.dot(LOSS,X)
+            
+            if(self.Regression =='Linear'):
+                LOSSX = np.dot(LOSS,X)
+            elif(self.Regression == 'L1'):
+                LOSSX = np.dot(LOSS,X) + self.Lambd1*np.sign(self.Weights)
+            elif(self.Regression == 'L2'):
+                LOSSX = np.dot(LOSS,X) + 2*self.Lambd2*self.Weights
+            else:
+                LOSSX = np.dot(LOSS,X)+ self.Lambd1*np.sign(self.Weights) + 2*self.Lambd2*self.Weights
             
             #Updating Parameters
             self.Weights -= (self.Lrate/N)*LOSSX
@@ -28,10 +53,19 @@ class LinearRegression:
 
     def Predict(self,X):
         A = np.dot(self.Weights,X.T) + self.Biases.copy()
-
         return A
+
+
+#Example Of Implementation
 
 X = np.random.rand(1000,5)
 Y = np.random.rand(1000,1)
+
 LR = LinearRegression(X,Y,0.01)
+print(LR.Predict(np.random.rand(5,5)))
+LR = LinearRegression(X,Y,0.01,Regression='L1',lambd1=0.01)
+print(LR.Predict(np.random.rand(5,5)))
+LR = LinearRegression(X,Y,0.01,Regression='L2',lambd2=0.01)
+print(LR.Predict(np.random.rand(5,5)))
+LR = LinearRegression(X,Y,0.01,Regression='ElasticNet',lambd1=0.01,lambd2=0.01)
 print(LR.Predict(np.random.rand(5,5)))
--- VisualML/LinearModels/LogisticRegression.py
+++ VisualML/LinearModels/LogisticRegression.py
@@ -1,18 +1,36 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self,X,Y,Lrate):
+    ''' 
+    Regression <type of Logistic Regression> ::
+    Logistic: Logistic Regression, 
+    L1: Lasso Logistic Regression, 
+    L2: Ridge Logistic Regression,
+    ElasticNet: ElasticNet Logistic Regression
+
+    lambd1: when [Regression='L1' || Regression='ElasticNet']
+    lambd2: when [Regression='L2' || Regression='ElasticNet']
+    '''
+    def __init__(self, X, Y, Lrate, Regression='Logistic', lambd1=0.01, lambd2=0.01):
         Feature = X.shape[1]
-        self.Weights = np.random.rand(1,Feature)
-        self.Biases = np.random.rand(1,1)
+
+        self.Regression = Regression
+        
+        #Hyper-Parameters Initiallization
         self.Threshold = 0.5
         self.Lrate = Lrate
         self.Epochs =10
-        
+        self.Lambd1 = lambd1
+        self.Lambd2 = lambd2
+
+        #Parameters Initiallization
+        self.Weights = np.random.rand(1,Feature)
+        self.Biases = np.random.rand(1,1)
+
         self.Train(X,Y)
 
     def Sigmoid(self,Z):
-        return 1/(1+np.e**-Z)
+        return 1/(1+np.exp(-Z))
     
     def Train(self,X,Y):
         N = X.shape[0]
@@ -24,7 +42,15 @@ class LogisticRegression:
             
             #Backward Propagation
             LOSS = Y.T-A
-            LOSSX = np.dot(LOSS,X)
+
+            if(self.Regression =='Logistic'):
+                LOSSX = np.dot(LOSS,X)
+            elif(self.Regression == 'L1'):
+                LOSSX = np.dot(LOSS,X) + self.Lambd1*np.sign(self.Weights)
+            elif(self.Regression == 'L2'):
+                LOSSX = np.dot(LOSS,X) + 2*self.Lambd2*self.Weights
+            else:
+                LOSSX = np.dot(LOSS,X)+ self.Lambd1*np.sign(self.Weights) + 2*self.Lambd2*self.Weights
             
             #Updating Parameters
             self.Weights += (self.Lrate/N)*LOSSX
@@ -34,11 +60,18 @@ class LogisticRegression:
         Z = np.dot(self.Weights,X.T) + self.Biases.copy()
         A = self.Sigmoid(Z)
 
-        # Add Threshold value
+        return (A >= self.Threshold).astype(int)
 
-        return A
+#Example of Implementation
 
 X = np.random.rand(1000,10)
-Y = np.random.rand(1000,1)
+Y = (np.random.randn(1000, 1) > 0.5).astype(int) 
+
 LR = LogisticRegression(X,Y,0.01)
-print(LR.Predict(np.random.rand(1,10)))
+print(LR.Predict(np.random.rand(5,10)))
+LR = LogisticRegression(X,Y,0.01,Regression='L1',lambd1=0.01)
+print(LR.Predict(np.random.rand(5,10)))
+LR = LogisticRegression(X,Y,0.01,Regression='L2',lambd2=0.01)
+print(LR.Predict(np.random.rand(5,10)))
+LR = LogisticRegression(X,Y,0.01,Regression='ElasticNet',lambd1=0.01,lambd2=0.01)
+print(LR.Predict(np.random.rand(5,10)))
